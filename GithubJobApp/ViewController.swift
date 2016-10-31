ffc4ceca4f4370ddc3e89513ec8516fb7bf920f5
Taylor Harrison <taylor@taylors-mac-pro.local>
2016-10-30 19:50:33 -0700

    Initial Commit

---
+++ GithubJobApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubJobApp
+//
+//  Created by Taylor Harrison on 10/30/16.
+//  Copyright © 2016 Taylor Harrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
