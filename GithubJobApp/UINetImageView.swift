2e561d7c897a0efe99efd14d70cbebdadf91459a
Taylor <taylor@oakcity.io>
2016-12-13 17:31:42 -0500

    image loading fix and consolidation

--- GithubJobApp/UINetImageView.swift
+++ GithubJobApp/UINetImageView.swift
@@ -13,16 +13,14 @@ extension UIImageView {
     
     func setImgFromUrl(url: NSURL?) {
         guard let url = url, rawUrl = url.path else {
+            setPlaceholder(){}
             return
         }
         if let image = NetImageCache.cache.objectForKey(rawUrl) as? UIImage {
             print("using cached image")
             self.image = image
         } else {
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
-                let uiImage = UIImage(named: "logo_placeholder")
-                dispatch_async(dispatch_get_main_queue()) { () -> Void in
-                    self.image = uiImage
+            setPlaceholder() {
                     let task = NSURLSession.sharedSession().dataTaskWithURL(url) {data, _, error in
                         if error == nil {
                             if let data = data, logoImage = UIImage(data: data) {
@@ -36,7 +34,16 @@ extension UIImageView {
                         }
                     }
                     task.resume()
-                }
+            }
+        }
+    }
+    
+    func setPlaceholder(completion: () -> Void) {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            let uiImage = UIImage(named: "logo_placeholder")
+            dispatch_async(dispatch_get_main_queue()) {
+                self.image = uiImage
+                completion()
             }
         }
     }
